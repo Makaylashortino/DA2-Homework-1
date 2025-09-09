6de11f936eb75341c03c4a9819d76400ccd161ac
Makaylashortino <makaylashortino@ufl.edu>
2025-09-09 00:46:23 +0000

    trying to debug predict function

--- mae.py
+++ mae.py
@@ -3,15 +3,15 @@ def MAE(true_labels, pred_labels):
     tot = 0
     for i in range(n):
        tot += abs(true_labels[i] - pred_labels[i])
-    mae = tot/ n   
-    return mae
+    mean_abs_error = tot/ n   
+    return mean_abs_error
 
 
 def predict(x):
     if x <= 0:
         #y = -1
-        return -1
+        return (-1)
     else:
         #y = 1
-        return 1
+        return (1)
     #return y
