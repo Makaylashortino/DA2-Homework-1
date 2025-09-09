296f253e4db42992d854062a19618227a72de5ef
Makaylashortino <makaylashortino@ufl.edu>
2025-09-09 00:22:46 +0000

    added predict function

--- mae.py
+++ mae.py
@@ -5,3 +5,11 @@ def MAE(true_labels, pred_labels):
        tot += abs(true_labels[i] - pred_labels[i])
     mae = tot/ n   
     return mae
+
+
+def predict(x):
+    if x <= 0:
+        y = -1
+    else:
+        y = 1
+    return y
